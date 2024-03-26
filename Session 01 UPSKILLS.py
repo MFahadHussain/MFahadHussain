06952d30fafac7a17a698d2c55d86c54dc2dfdf6
Fahad Hussain <162036149+MFahadHussain@users.noreply.github.com>
2024-03-26 05:46:05 +0500

    Add files via upload

---
+++ Session 01 UPSKILLS.py
@@ -0,0 +1,375 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+name="Fahad"
+print("My Name is "+name)
+
+
+# In[5]:
+
+
+name1 =input("what is your name ? ")
+
+
+# In[6]:
+
+
+print(type(name))
+
+
+# In[ ]:
+
+
+"#Interger\n",
+    "a = 3\n",
+    "print(a)"
+
+
+# In[ ]:
+
+
+"name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "3\n"
+
+
+# # Data Types
+
+# In[ ]:
+
+
+String = "Hello World"
+Boolean  = True or False
+Float = 4.5,
+Integer  = 13
+   
+
+
+# # ARITHEMETIC OPERATION
+
+# In[86]:
+
+
++
+-
+*
+**
+()
+//
+get_ipython().run_line_magic('', '')
+
+
+# In[87]:
+
+
+x=10
+y=20
+
+
+# In[11]:
+
+
+print("Addition",x+y)
+
+
+# In[12]:
+
+
+print("Subtraction",x-y)
+
+
+# In[13]:
+
+
+print("Multiply",x*y)
+
+
+# In[14]:
+
+
+print("Divide",x/y)
+
+
+# In[16]:
+
+
+print("Integer Division",x//y)
+
+
+# In[17]:
+
+
+print("Modulus",x%y)#reminder
+
+
+# In[18]:
+
+
+print("Exponential",x**y)
+
+
+# # COMPARISON OPERATERS
+
+# In[20]:
+
+
+x>y
+
+
+# In[21]:
+
+
+x<y
+
+
+# In[88]:
+
+
+x>=y
+
+
+# In[89]:
+
+
+x>=y
+
+
+# In[22]:
+
+
+x==y
+
+
+# In[23]:
+
+
+x!=y
+
+
+# # DATA STRUCTURE
+
+# In[ ]:
+
+
+STRING
+LIST
+TUPLE
+STRING
+SET
+
+
+# In[29]:
+
+
+e='Apple'
+e
+
+
+# In[38]:
+
+
+Fruits=["Apple","Banana","Orange","PineApple"]
+Fruits
+
+
+# In[39]:
+
+
+Fruits[1]
+
+
+# In[40]:
+
+
+Fruits[-1]
+
+
+# In[41]:
+
+
+Fruits.append("Coconut")
+
+
+# In[44]:
+
+
+Fruits
+
+
+# In[45]:
+
+
+Fruits.remove("Coconut")
+
+
+# In[46]:
+
+
+Fruits
+
+
+# In[47]:
+
+
+Fruits.insert(1,"Coconut")
+
+
+# In[49]:
+
+
+Fruits.sort()
+Fruits
+
+
+# In[52]:
+
+
+Fruits.reverse()
+Fruits
+
+
+# In[53]:
+
+
+Fruits.count("Coconut")
+
+
+# In[54]:
+
+
+Fruits.index("Coconut")
+
+
+# # TUPLE
+
+# In[59]:
+
+
+my_tuple = (1,2,"Apple",3,True,4,5,6)
+my_tuple
+
+
+# In[60]:
+
+
+my_tuple[2]
+
+
+# In[61]:
+
+
+my_tuple[0:7]
+
+
+# In[62]:
+
+
+my_tuple[0:-2]
+
+
+# In[63]:
+
+
+my_tuple[0:6:2]
+
+
+# # LEN
+
+# In[66]:
+
+
+length=len(my_tuple)
+length
+
+
+# # SETS : mutable but no duplicates allowed
+
+# In[71]:
+
+
+my_set= {7,"Apple",28,19,11}
+my_set
+
+
+# In[72]:
+
+
+my_set.add(4)
+
+
+# In[73]:
+
+
+my_set.remove('Apple')
+
+
+# In[74]:
+
+
+my_set
+
+
+# In[76]:
+
+
+set1={2,4,6,8}
+set2={1,3,4,5,7,9}
+
+
+# In[78]:
+
+
+#UNIION
+set1 | set2
+
+
+# In[80]:
+
+
+#INTERSECTION
+set1 & set2
+
+
+# In[81]:
+
+
+string="Fahad"
+
+
+# In[82]:
+
+
+string.upper()
+
+
+# 
+# # Dictionary
+
+# In[83]:
+
+
+Grades = {"Fahad":[40,20,100],"Saira":[44,22,99]}
+Grades
+
+
+# In[85]:
+
+
+Grades["Saira"][1]
+
+
+# In[ ]:
+
+
+
+
